42382aa151538e4a68134a340c1532691ccaf44c
Qabas al ani <doctorqa@yahoo.com>
2021-08-20 15:42:38 -0400

    i have created seeds.sql that contains the department, employee and role

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,24 @@
-DROP DATABASE IF EXISTS employee_db;
+
 
 CREATE DATABASE employee_DB;
 
 USE employee_db;
 
 CREATE TABLE department(
-id INT NOT NULL PRIMARY KEY,
-name VARCHAR(30) NOT NULL
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30)
 );
 
 CREATE TABLE role(
-id INT PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT,
-FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL  
+FOREIGN KEY (department_id) REFERENCES department(id)  
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
