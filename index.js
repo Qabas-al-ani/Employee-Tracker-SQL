fd6ba360b0a2d0e0e0613245a91ca85605ac116a
Qabas al ani <doctorqa@yahoo.com>
2021-08-20 16:31:22 -0400

    i have created my data base connection to lead to and show if there is an error or not and set up the prompt function to have a specific question needed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// importing needed modules 
+// importing needed modules
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
@@ -11,3 +11,32 @@ const dbConnection = mysql.createConnection({
   password: "0000",
   database: "employee_db",
 });
+
+// set up the data base connection to show if there is an err or not and fire the prompStarter function
+dbConnection.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to database");
+  promptStarter();
+});
+
+// set up the prompt function to have a specific question needed 
+const promptStarter = () => {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        "View All Employees?",
+        "Add Employee?",
+        "Update Employee Role",
+        "View All Roles?",
+        "Add Role?",
+        "View all Departments",
+        "Add Department?",
+      ],
+    },
+  ]);
+};
